1b830a44b3b62a9fcd81f7cdf49b171f95eb18a9
ljulia <Susvin01>
2017-06-08 12:57:02 -0300

    adaptando el server

--- server/server.js
+++ server/server.js
@@ -1,18 +1,24 @@
 var http = require("http");
 var server = http.createServer(handleRequests);
 var port = 11111;
-var dbconnect = require("./db/dbconnect.js");
-var serverFunctions = require("./serverFunctions/serverFunctions.js");
-var queryString = require("querystring");
+/*var dbconnect = require("./db/dbconnect.js");*/
+/*var serverFunctions = require("./serverFunctions/serverFunctions.js");*/
+/*var queryString = require("querystring");*/
+var url = require('url');
 
 function handleRequests(request,response) {
 	try {
-	  var toExecute = serverFunctions.callbackFromRequest(request);
-	  toExecute(queryString.parse(request.url.replace(/^.*\?/, '')), function(err,result) {
+	  /*var toExecute = serverFunctions.callbackFromRequest(request);
+	  toExecute(request.url, function(err,result) {
 		response.setHeader("Content-Type","application/json");
 		response.end(JSON.stringify(result));
-	  });
-	}catch(ex) {
+	  });*/
+	  	response.writeHead(200, {'Content-Type': 'text/html'});
+	  	var adr = request.url;
+	  	var q = url.parse(adr, true);
+	  	response.write(q.pathname);
+	  	response.end();
+	} catch(ex) {
 		console.log("Uh " + ex);
 		response.statusCode = 500;
 		response.end("Se rompió, perdón");
